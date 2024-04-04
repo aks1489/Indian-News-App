800505b0e33bdee525e514c1eb995666b2642b8c
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-04-04 21:20:57 +0530

    marging with fetched data

--- src/components/News/NewsCard.tsx
+++ src/components/News/NewsCard.tsx
@@ -1,4 +1,4 @@
-import LoadingAnimation from "../../assets/spinning-dots.svg"
+import LoadingAnimation from "../../assets/notFound.svg"
 export default function NewsCard(props: any) {
 
     const article = props.article;
@@ -14,19 +14,17 @@ export default function NewsCard(props: any) {
     return (
         <div className="card p-0" style={{ width: "18rem" }} data-bs-theme="dark">
             <img src={article.urlToImage ? article.urlToImage : LoadingAnimation} className="card-img-top card_img" alt={article.title} />
-            <div className="card-body p-1">
+            <div style={{maxHeight: "12.5rem"}} className="card-body p-1">
                 <h5 className="card-title">{article.title}</h5>
-                <p className="card-text">{article.description}</p>
+                <div data-bs-spy="scroll" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" className="scrollspy-example bg-body-tertiary p-3 rounded-2 scroller" tabIndex={0}>
+                    <p className="artical_description">{article.description ? article.description : "Go to the source site to see the full news"}</p>
+                </div>
             </div>
             <ul className="list-group list-group-flush">
                 <li className="list-group-item">An item</li>
                 <li className="list-group-item">A second item</li>
                 <li className="list-group-item">A third item</li>
             </ul>
-            <div className="card-body">
-                <a href="#" className="card-link">Card link</a>
-                <a href="#" className="card-link">Another link</a>
-            </div>
         </div>
     );
 }
